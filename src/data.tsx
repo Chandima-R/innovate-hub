8ccd48697ce8c5aaf2b542960abd19fa73276ba3
Chandima <chandimar.online@gmail.com>
2024-08-26 09:41:54 +0530

    feat: add all images

---
+++ src/data.tsx
@@ -0,0 +1,35 @@
+export const tableData = [
+  {
+    brandame: "wix  ",
+    brandLogo: "",
+    description: "",
+    members: [
+      {
+        memberName: "",
+        image: "",
+        comments: [
+          {
+            commentText: "",
+          },
+        ],
+      },
+    ],
+    categories: [
+      {
+        categoryName: "",
+        categoryColor: "",
+      },
+    ],
+    tags: [
+      {
+        tagName: "",
+      },
+    ],
+    meetings: [
+      {
+        meetingName: "",
+        meetingColor: "",
+      },
+    ],
+  },
+];
